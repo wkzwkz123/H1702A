a6955a5040c909c8ec5d641b2cfa46a6aba5eb8e
dell <1091838518@qq.com>
2017-09-12 09:53:41 +0800

    首次提交

---
+++ app/src/main/java/com/jiyun/day02_h1702a/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jiyun.day02_h1702a;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
